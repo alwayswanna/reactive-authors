f22dcc215ca241ce8c6c252ab20e1803c0dcba60
agleb <a.gleb@yahoo.com>
2021-06-26 21:51:39 +0500

    - migration;

--- src/main/java/a/gleb/reactiveauthors/dto/AdministrativeAccount.java
+++ src/main/java/a/gleb/reactiveauthors/dto/AdministrativeAccount.java
@@ -1,22 +1,58 @@
 package a.gleb.reactiveauthors.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.MappedCollection;
 import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
 
 @Table("administration")
 @Data
 @NoArgsConstructor
-public class AdministrativeAccount {
+public class AdministrativeAccount implements UserDetails {
 
     @Id
     private Long id;
-    private String userName;
+    private String username;
+    @JsonIgnore
+    private String password;
     private String name;
     private String surname;
     private String email;
-    private String password;
+    private boolean active;
+    @MappedCollection(idColumn = "authority", keyColumn = "authority_id")
+    private Set<Role> roles;
 
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
--- src/main/resources/db/migration/V1__initial__db.sql
+++ src/main/resources/db/migration/V1__initial__db.sql
@@ -1,8 +1,30 @@
+create table posts
+(
+    id          bigserial primary key,
+    title       varchar(1024),
+    description varchar(2048),
+    post_text   varchar(4096),
+    likes       int8
+);
 
-create table posts (
-    id bigserial primary key,
-    title varchar (1024),
-    description varchar (2048),
-    post_text varchar (4096),
-    likes int8
-)
+
+create table administration
+(
+    id       bigserial primary key,
+    username varchar(2048),
+    password varchar(2048),
+    name     varchar(2048),
+    surname  varchar(2048),
+    email    varchar(2048),
+    role     varchar(64)
+);
+
+create table user_role
+(
+    user_id int8 not null,
+    roles   varchar(255)
+);
+
+alter table if exists user_role
+    add constraint user_role_fk
+    foreign key (user_id) references administration;
