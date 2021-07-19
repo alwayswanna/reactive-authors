64c0d5e1569e0c0e29a11835c0489ed96bc1360b
alwayswanna <a.gleb@yahoo.com>
2021-07-19 08:20:17 -0700

    + single view. Cast Future -> to Object;

--- authors_client/lib/views/single_post.dart
+++ authors_client/lib/views/single_post.dart
@@ -1,4 +1,5 @@
 
+
 import 'package:authors_client/configuration/connection.dart';
 import 'package:authors_client/models/post.dart';
 import 'package:flutter/cupertino.dart';
@@ -15,45 +16,52 @@ class SinglePostState extends State<SingleView>{
   static const routeName = '/single-story';
 
 
-  Post loadPostById(id) {
-    Post ps = Connection().getSelectedPost(id).then((value) => post) as Post;
+  Future<Post> loadPostById(id) {
+    Future<Post> ps = Connection().getSelectedPost(id);
     return ps;
   }
 
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments;
-    Post post = loadPostById(args.toString());
     return Scaffold(
       appBar: AppBar(
         title: Text('Reactive authors'),
       ),
       body: Form(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              width: 300,
-              child: Text(post.title),
-            ),
-            Container(
-              width: 300,
-              child: Text(post.description),
-            ),
-            Container(
-              width: 300,
-              child: Text(post.postText),
-            )
-          ],
-        ),
+        child: FutureBuilder<Post>(
+          future: loadPostById(args.toString()),
+          builder: (context, snapshot){
+            if(snapshot.hasData){
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    width: 300,
+                    child: Text(snapshot.data!.title),
+                  ),
+                  Container(
+                    width: 300,
+                    child: Text(snapshot.data!.description),
+                  ),
+                  Container(
+                    width: 300,
+                    child: Text(snapshot.data!.postText),
+                  )
+                ],
+              );
+            }else if(snapshot.hasError){
+              return Text("${snapshot.error}");
+            }
+            return CircularProgressIndicator();
+          },
+        )
 
       ),
     );
   }
-
 }
-
 
 class SingleView extends StatefulWidget {
   createState() => SinglePostState();
