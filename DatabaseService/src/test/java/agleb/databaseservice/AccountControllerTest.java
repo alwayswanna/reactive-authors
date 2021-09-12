72016e01ca9ce06c166fa567cec295bc80f4ac36
alwayswanna <a.gleb@yahoo.com>
2021-09-12 14:02:49 +0500

    + tests for db_service;

--- DatabaseService/src/test/java/agleb/databaseservice/AccountControllerTest.java
+++ DatabaseService/src/test/java/agleb/databaseservice/AccountControllerTest.java
@@ -16,8 +16,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -72,18 +71,79 @@ public class AccountControllerTest {
     }
 
     @Test
-    public void editAddedAccountTest() throws Exception{
+    public void editAddedAccountTest() throws Exception {
+        MvcResult result = mock.perform(get("/db_service/accounts"))
+                .andExpect(status().isOk())
+                .andReturn();
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<AccountDTO> accountDTOS = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<AccountDTO>>() {
+        });
+
+        AccountDTO accountDTO = accountDTOS.get(accountDTOS.size() - 1);
+
+        int index = Math.toIntExact(accountDTO.getId());
+        accountDTO.setUsername("Changed Test Username");
+        accountDTO.setPassword("Changed Test Password");
+        accountDTO.setName("Changed Test Name");
+        accountDTO.setSurname("Changed Test Surname");
+        accountDTO.setEmail("ChangedEmail@test.com");
+
+        String jsonBody = objectMapper.writeValueAsString(accountDTO);
+
+        this.mock.perform(put("/db_service/account/" + index)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonBody)
+                        .characterEncoding("utf-8"))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+
+    @Test
+    public void checkEditAccountTest() throws Exception {
+        MvcResult result = mock.perform(get("/db_service/accounts"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<AccountDTO> accountDTOS = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<AccountDTO>>() {
+        });
+        AccountDTO accountDTO = new AccountDTO(null, "Changed Test Username", "Changed Test Password", "Changed Test Name",
+                "Changed Test Surname", "ChangedEmail@test.com", true, RoleDTO.ROLE_ADMINISTRATOR, null);
+
+        Long id = accountDTOS.get(accountDTOS.size() - 1).getId();
+
+        this.mock.perform(get("/db_service/account/" + id))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("username", is(accountDTO.getUsername())))
+                .andExpect(jsonPath("password", is(accountDTO.getPassword())))
+                .andExpect(jsonPath("name", is(accountDTO.getName())))
+                .andExpect(jsonPath("surname", is(accountDTO.getSurname())))
+                .andExpect(jsonPath("email", is(accountDTO.getEmail())))
+                .andExpect(jsonPath("active", is(accountDTO.isActive())))
+                .andExpect(jsonPath("roleDTO", is(accountDTO.getRoleDTO())))
+
+        ;
     }
 
     @Test
-    public void checkEditAccountTest() throws Exception{
+    public void deleteAddedTestAccountTest() throws Exception {
+        MvcResult result = mock.perform(get("/db_service/accounts"))
+                .andExpect(status().isOk())
+                .andReturn();
 
-    }
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<AccountDTO> accountDTOS = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<AccountDTO>>() {
+        });
 
-    @Test
-    public void deleteAddedTestAccountTest() throws Exception{
+        Long id = accountDTOS.get(accountDTOS.size() - 1).getId();
 
+        this.mock.perform(delete("/db_service/account/" + id))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
 }
